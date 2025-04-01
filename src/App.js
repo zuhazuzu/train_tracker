c3ddbcece50426c0999b30087da4edf5918b2567
zuhazu <mehukatti1337@gmail.com>
2025-04-01 14:58:43 +0300

    Koomentit App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import MapComponent from './MapComponent';
 import './App.css';
-
+// app.js tiedosto, joka toimii sovelluksen pääkomponenttina
 function App() {
+    // state muuttuja hakusanalle
     const [searchTerm, setSearchTerm] = useState('');
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Junien Sijainnit</h1>
+                {/* input kenttä junan numeron hakemiseen */}
                 <input
                     type="text"
                     placeholder="Hae junan numerolla"
@@ -16,6 +18,7 @@ function App() {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </header>
+            {/* MapComponent, joka näyttää junien sijainnit kartalla */}
             <MapComponent searchTerm={searchTerm} />
         </div>
     );
